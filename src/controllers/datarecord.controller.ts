564a36e246787169387e086110a306e05bdd9ea8
LJstudent <poepjan123_2@hotmail.com>
2023-01-25 17:55:14 +0100

    add approves to controller

--- src/controllers/datarecord.controller.ts
+++ src/controllers/datarecord.controller.ts
@@ -3,6 +3,24 @@ import { AppDataSource } from "../data-source";
 import { DataRecord } from "../entity/DataRecord";
 
 export const getDataRecords = async function (req: Request, res: Response) {
-    const users = await AppDataSource.getRepository(DataRecord).find()
-    res.json(users)
+    const dataRecords = await AppDataSource.getRepository(DataRecord).find()
+    res.json(dataRecords)
+}
+
+export const putApprovedByDirector = async function (req: Request, res: Response) {
+    const dataRecord = await AppDataSource.getRepository(DataRecord).findOneBy({
+        id: Number(req.params.id),
+    })
+    AppDataSource.getRepository(DataRecord).merge(dataRecord, req.body)
+    const results = await AppDataSource.getRepository(DataRecord).save(dataRecord)
+    return res.send(results)
+}
+
+export const putApprovedByHeadOfPurchasingDepartment = async function (req: Request, res: Response) {
+    const dataRecord = await AppDataSource.getRepository(DataRecord).findOneBy({
+        id: Number(req.params.id),
+    })
+    AppDataSource.getRepository(DataRecord).merge(dataRecord, req.body)
+    const results = await AppDataSource.getRepository(DataRecord).save(dataRecord)
+    return res.send(results)
 }
--- src/routes/datarecord.routes.ts
+++ src/routes/datarecord.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getDataRecords } from "../controllers/datarecord.controller";
+import { getDataRecords, putApprovedByDirector, putApprovedByHeadOfPurchasingDepartment } from "../controllers/datarecord.controller";
 
 const router = Router();
 
 // register routes
 router.get('', getDataRecords)
+router.put('/approved-director/:id', putApprovedByDirector)
+router.put('/approved-headofpurchasingdepartment/:id', putApprovedByHeadOfPurchasingDepartment)
 
 export default router
